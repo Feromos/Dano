3d2ba33dd845566a17774f15d34b22a1707d8128
Alister111 <alekcandr200468@gmail.com>
2021-12-01 21:07:10 +0300

    second option

--- grapth.py
+++ grapth.py
@@ -3,9 +3,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy
 
-df_1 = pd.read_excel('разводы.xlsx', index_col='time')
-df_2 = pd.read_excel('браки.xlsx', index_col='time')
-df_3 = pd.DataFrame({'число разводов': df_1['Белгородская область'],
+df_1 = pd.read_excel('Tables/жилье_1.xlsx', index_col='time')
+df_2 = pd.read_excel('Tables/браки.xlsx', index_col='time')
+df_3 = pd.DataFrame({'цены на первичном рынку жилья': df_1['Белгородская область'],
                      'Число браков': df_2['Белгородская область']})
 df_3.plot()
 plt.show()
