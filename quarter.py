c7d51cd286e9a851e1a5e3bf41edaa4ff0dfe960
Alister111 <alekcandr200468@gmail.com>
2021-12-05 19:41:14 +0300

    second option

--- quarter.py
+++ quarter.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy
 
-df = pd.read_excel('1 StatSspace/result_dem3_col7.xlsx', index_col='region')
+df = pd.read_excel('1 StatSspace/result_trud2_col1.xlsx', index_col='region')
 a = ['1 квартал 2015', '2 квартал 2015', '3 квартал 2015', '4 квартал 2015', '1 квартал 2016', '2 квартал 2016',
      '3 квартал 2016', '4 квартал 2016', '1 квартал 2017', '2 квартал 2017', '3 квартал 2017',
      '4 квартал 2017', '1 квартал 2018', '2 квартал 2018', '3 квартал 2018', '4 квартал 2018',
@@ -15,7 +15,7 @@ for i in range(0, len(df.columns), 3):
         k = 0
         t = 3
         for m in range(3):
-            if df[df.columns[i + m]][j] > 0:
+            if i + m < len(df.columns) and df[df.columns[i + m]][j] > 0:
                 k += df[df.columns[i + m]][j]
             else:
                 t -= 1
@@ -29,6 +29,6 @@ for i in range(0, len(df.columns), 3):
 if numpy.NAN in d:
     d.pop(numpy.NAN)
 df_1 = pd.DataFrame(d)
-df_1.to_excel('Tables/разводы по кварталам.xlsx', index=False)
+df_1.to_excel('Tables/безработица по кварталам.xlsx', index=False)
 # df_1.plot()
 # plt.show()
